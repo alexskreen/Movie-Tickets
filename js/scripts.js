b446a5e52200c43b111ace2d4454b6372e39bb87
Student <example@example.com>
2020-01-28 16:01:12 -0800

    get ticket price to add up and console log correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,113 @@
 
 //business logic
-function ticketBook() {
-    this.tickets = [],
-    this.currentId = 0
-}
+function TicketBook() {
+    this.tickets = []
+};
 
-ticketBook.prototype.addTicket = function (ticket) {
-    ticket.id = this.assignId();
+TicketBook.prototype.addTicket = function (ticket) {
     this.tickets.push(ticket);
 }
 
-ticketBook.prototype.assignId = function (ticket) {
-    this.currentId += 1;
-    return this.currentId;
-}
-
-ticketBook.prototype.findTicket = function (id) {
-    for (var i = 0; i < this.tickets.length; i++) {
-        if (this.tickets[i]) {
-            if (this.tickets[i].id == id) {
-                return this.tickets[i];
-            }
-        }
-    };
-    return false;
-}
-
-ticketBook.prototype.deleteTicket = function (id) {
-    for (var i = 0; i < this.tickets.length; i++) {
-        if (this.tickets[i]) {
-            if (this.tickets[i].id == id) {
-                delete this.tickets[i];
-                return true;
-            }
-        }
-    };
-    return false;
-}
-function Ticket(movies, times, ages) {
+function Ticket(movies, times, ages, price) {
     this.movies = movies
     this.times = times
     this.ages = ages
-};
-var ticketBook = new ticketBook();
-
-function displayTicketDetails(ticketBookToDisplay) {
-    var ticketsList = $("ul#tickets");
-    var htmlForTicketInfo = "";
-    ticketBookToDisplay.tickets.forEach(function (ticket) {
-        htmlForTicketInfo += "<li id=" + ticket.id + ">" + ticket.movies + " " + ticket.times + " " + ticket.ages + "</li>";
-    });
-    ticketsList.html(htmlForTicketInfo);
+    this.price = price
 };
 
-function showTicket(ticketId) {
-    var ticket = ticketBook.findTicket(ticketId);
-    $(".priceOfTicket").show();
-    $(".movies").html(ticket.movies);
-    $(".times").html(ticket.times);
-    $(".ages").html(ticket.ages);
-};
+// function displayTicketDetails(ticketBookToDisplay) {
+//     var ticketsList = $("ul#tickets");
+//     var htmlForTicketInfo = "";
+//     ticketBookToDisplay.tickets.forEach(function (ticket) {
+//         htmlForTicketInfo += "<li id=" + ticket.id + ">" + ticket.movies + " " + ticket.times + " " + ticket.ages + "</li>";
+//     });
+//     ticketsList.html(htmlForTicketInfo);
+// };
 
-function attachTicketListeners() {
-    $("ul#tickets").on("click", "li", function () {
-      showTicket(this.id);
-    });
-    $("#show-ticket").hide();
-    displayTicketDetails(ticketBook);
-  }
+// function showTicket(ticketId) {
+//     var ticket = ticketBook.findTicket(ticketId);
+//     $(".priceOfTicket").show();
+//     $(".movies").html(ticket.movies);
+//     $(".times").html(ticket.times);
+//     $(".ages").html(ticket.ages);
+// };
+
+// function attachTicketListeners() {
+//     $("ul#tickets").on("click", "li", function () {
+//         showTicket(this.id);
+//     });
+//     $("#show-ticket").hide();
+//     displayTicketDetails(ticketBook);
+// }
+
+// ticketPrice.tickets.forEach(function (ticket) {
+//     if (ticket.movie === "fastnFurious") {
+//         ticketPriceTotal + 10
+//     }
+//     else if (ticket.movie === "codeCoder") {
+//         ticketPriceTotal + 5
+//     }
+// }
+
 
 //front-end
 $(document).ready(function () {
-    attachTicketListeners();
+
+    var ticketBook = new TicketBook();
+    // attachTicketListeners();
+
     $("#movieForm").submit(function (event) {
         event.preventDefault();
-        //gather value from form
         var movies = $("select#movies").val();
         var times = $("select#times").val();
         var ages = $("select#ages").val();
-        var newTicket = new Ticket(movies, times, ages)
+        
+        var price = 0
+
+// AGES CALCULATIONS
+        if (ages === "6") {
+            price = price -= 4
+        }
+        if (ages === "8"){
+            price = price +=0
+        }
+        if (ages === "10"){
+            price = price +=4
+        }
+        if (ages === "12"){
+            price = price +=8
+        }
+// // TIMES CALCULATIONS
+        if (times === "6") {
+            price = price -= 4
+        }
+        if (times === "8"){
+            price = price +=0
+        }
+        if (times === "10"){
+            price = price +=4
+        }
+        if (times === "12"){
+            price = price +=8
+        }
+// // TIMES CALCULATIONS
+        if (movies === "6") {
+            price = price - 4
+        }
+        if (movies === "8"){
+            price = price +0
+        }
+        if (movies === "10"){
+            price = price +4
+        }
+        if (movies === "12"){
+            price = price +8
+        }
+        // console.log(price);
+        var newTicket = new Ticket(movies, times, ages, price)
         ticketBook.addTicket(newTicket);
-        displayTicketDetails(ticketBook);
+        console.log(ticketBook.tickets)
+        // displayTicketDetails(ticketBook);
         //console.log(movies, times, ages);
     });
 });
